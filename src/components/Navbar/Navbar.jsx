b645c9e80bb4285a7924026651d612df0b48c47d
walid-official <walid.official8@gmail.com>
2025-02-20 09:31:45 +0600

    firebase enviroment setup is done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import { Link, NavLink } from "react-router";
 const Navbar = () => {
   const Links = (
     <>
-      <NavLink className="font-semibold">Home</NavLink>
-      <NavLink className="btn">Login</NavLink>
+      <NavLink to="/" className="font-semibold">Home</NavLink>
+      <NavLink className="btn">Google Login</NavLink>
     </>
   );
   return (
     <div className="bg-gray-200 px-10">
       <div className="navbar">
         <div className="flex-1">
-          <h2 className="text-xl font-bold">PlanPilot</h2>
+          <h2 className="text-2xl font-bold">PlanPilot</h2>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal space-x-4 items-center px-1">
