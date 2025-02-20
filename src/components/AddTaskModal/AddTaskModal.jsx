3887f8656e2aeb26d34dc81fa2d669e74950971c
walid-official <walid.official8@gmail.com>
2025-02-20 08:53:49 +0600

    form is added

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -6,10 +6,44 @@ const AddTaskModal = () => {
       {/* Open the modal using document.getElementById('ID').showModal() method */}
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
+          <div className="">
+            <div className="card">
+              <form className="card-body">
+                <div className="form-control">
+                  <div className=" py-2">Title</div>
+                  <input
+                    type="text"
+                    placeholder="Title"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <div className="  py-2">Description</div>
+                  <input
+                    type="text"
+                    placeholder="Description"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <div className="py-2">Your Category</div>
+                  <select className="select select-bordered w-full max-w-xs">
+                    <option disabled selected>
+                      Your Category
+                    </option>
+                    <option>To Do</option>
+                    <option>In Progress</option>
+                    <option>Done</option>
+                  </select>
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Add Task</button>
+                </div>
+              </form>
+            </div>
+          </div>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button in form, it will close the modal */}
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Tasks = () => {
-    return (
-        <div className='w-11/12 mx-auto'>
-            <div className="flex gap-10 justify-between">
-                <div className="bg-gray-200 w-[50%] min-h-[300px]"></div>
-                <div className="bg-gray-200 w-[50%] min-h-[300px]"></div>
-            </div>
-            <div className="flex justify-center my-10">
-            <div className="bg-gray-200 w-[50%] min-h-[300px]"></div>
-            </div>
+  return (
+    <div className="w-11/12 mx-auto">
+      <div className="flex gap-10 justify-between">
+        <div className="bg-gray-200 w-[50%] min-h-[300px]">
+          <div className="font-bold text-center text-4xl py-3 border btn flex justify-center">
+            To-Do
+          </div>
         </div>
-    );
+        <div className="bg-gray-200 w-[50%] min-h-[300px]">
+          <div className="font-bold text-center text-4xl py-3 border btn flex justify-center">
+            In Progress
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center my-10">
+        <div className="bg-gray-200 w-[50%] min-h-[300px]">
+          <div className="font-bold text-center text-4xl py-3 border btn flex justify-center">
+            Done
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tasks;
